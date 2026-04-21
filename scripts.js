65c8b2ce321fe82320e3d1f1fea67a8c713c3402
ChrisCP-tech <chrisp.apple00@gmail.com>
2026-04-21 11:52:30 -0700

    Update image URL for Mob Psycho 100 II

--- scripts.js
+++ scripts.js
@@ -94,7 +94,7 @@ const animeData = [
     year: 2019,
     score: 8.95,
     synopsis: "Shigeo 'Mob' Kageyama continues suppressing his overwhelming psychic powers while striving to grow as a person and protect the people who matter to him.",
-    image: "https://myanimelist.net/anime/37510/Mob_Psycho_100_II/pics"
+    image: "https://myanimelist.net/images/anime/1918/96303l.jpg"
   },
   {
     title: "Bleach: Thousand-Year Blood War",
